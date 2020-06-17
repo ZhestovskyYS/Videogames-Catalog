24b50cd5b16a2d3cd28dc9d0018256902d403298
ZhestovskyYS <61592205+ZhestovskyYS@users.noreply.github.com>
2020-06-17 10:25:19 +0500

    Update actions.kt

--- src/main/kotlin/redux/actions.kt
+++ src/main/kotlin/redux/actions.kt
@@ -2,7 +2,7 @@ package redux
 
 class AddGame: RAction
 class AddGenre: RAction
-class AddToPossessed(val index:Int): RAction
+class AddToPossessed(val id:Int): RAction
 class DeleteGame(val id: Int): RAction
 class DeleteGenre(val id: Int): RAction
 class EditGenre(val id: Int): RAction
